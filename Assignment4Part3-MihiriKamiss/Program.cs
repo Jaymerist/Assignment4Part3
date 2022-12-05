ebcb8fdb86b275537dfab6425885c4c1ed9b0f67
Jaymerist <76460057+Jaymerist@users.noreply.github.com>
2022-12-05 13:07:10 -0700

    Add files via upload

---
+++ Assignment4Part3-MihiriKamiss/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Assignment4Part3_MihiriKamiss
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
